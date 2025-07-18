a8b9340c1f3f3f00a7abcb17ff98428c78a41243
Anthony Goncalves <antgoncj@amazon.com>
2025-07-18 14:20:30 -0400

    Attempt correct env var setting and push more responsibility onto the shell script

--- docker_image_resources/tests/scripts/scan-image.sh
+++ docker_image_resources/tests/scripts/scan-image.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
+SCAN_OUTPUT=$(trivy image --ignorefile docker_image_resources/tests/.trivyignore --no-progress --severity CRITICAL,HIGH,MEDIUM,LOW $IMAGE_REFERENCE --format json)
+
 # Initialize counts with default values
 CRITICAL_COUNT=0
 HIGH_COUNT=0
